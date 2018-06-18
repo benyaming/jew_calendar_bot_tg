1b624c46783f69345441e0a503a636c34d57baa1
Benyomin Ginzburg <benyomin.94@gmail.com>
2018-06-19 00:51:20 +0300

    dev

--- bot.py
+++ bot.py
@@ -50,7 +50,6 @@ def webhook():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
-    return 'ok'
 
 
 @bot.message_handler(commands=['start'])
