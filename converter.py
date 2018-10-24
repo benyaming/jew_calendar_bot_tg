f7107d451fe3b7ea89c1db47b43b4f087097e9f7
Simon <chechin.simon@gmail.com>
2018-10-24 23:51:32 +0300

    WIP

--- converter.py
+++ converter.py
@@ -32,21 +32,28 @@ def convert_heb_to_greg(hebrew_date: tuple, lang: str) -> dict:
     day = hebrew_date[2]
     year_is_leap = Year(year).leap
     try:
+        if year_is_leap:
+            print('yoy')
+            if (month == 12) or (month == 13):
+                hebrew_date_2 = (
+                    hebrew_date[0],
+                    month+1,
+                    hebrew_date[2]
+                )
+                print('uhu')
+                greg_date_1 = HebrewDate(hebrew_date).to_greg().tuple()
+                day_of_week_1 = datetime(*greg_date_1).weekday()
+                greg_date_2 = HebrewDate(hebrew_date_2).to_greg().tuple()
+                day_of_week_2 = datetime(*greg_date_2).weekday()
+                response = dict()
+                response['response'] = localization.Converter.convert_heb_to_greg_two(
+                    hebrew_date, hebrew_date_2,
+                    day_of_week_1, day_of_week_2,
+                    greg_date_1, greg_date_2,
+                    lang
+                )
         greg_date = HebrewDate(year, month, day).to_greg().tuple()
         day_of_week = datetime(*greg_date).weekday()
-        if year_is_leap:
-            if month == 12:
-                hebrew_date = (
-                    hebrew_date[0],
-                    13,
-                    hebrew_date[2]
-                )
-            elif month == 13:
-                hebrew_date = (
-                    hebrew_date[0],
-                    14,
-                    hebrew_date[2]
-                )
         response = dict()
         response['response'] = localization.Converter.convert_heb_to_greg(
             hebrew_date,
