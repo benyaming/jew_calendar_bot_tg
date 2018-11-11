c6f0017bd759093a1e29d06d0248c50865f94f32
Simon <chechin.simon@gmail.com>
2018-11-12 02:30:53 +0300

    beta 0.1

--- states.py
+++ states.py
@@ -2,8 +2,12 @@ import psycopg2
 
 import settings
 
- 
+from utils import get_tz_by_location
+from utils import log
+
+
 def check_state(user: int) -> dict:
+    log(f'Database\tcheck_state\tSTART\t{user}')
     with psycopg2.connect(settings.db_parameters_string) as conn:
         cur = conn.cursor()
         check_query = f'SELECT state FROM states ' \
@@ -18,10 +22,12 @@ def check_state(user: int) -> dict:
                 'ok': True,
                 'state': user_has_state[0]
             }
+        log(f'Database\tcheck_state\tEND\t{user}')
         return state_dict
 
 
 def set_state(user: int, state: str) -> None:
+    log(f'Database\tset_state\tSTART\t{user}')
     with psycopg2.connect(settings.db_parameters_string) as conn:
         cur = conn.cursor()
         check_query = f'SELECT * FROM states WHERE user_id = {user}'
@@ -32,11 +38,14 @@ def set_state(user: int, state: str) -> None:
                              f'VALUES ({user}, \'{state}\')'
             cur.execute(set_time_query)
             conn.commit()
+        log(f'Database\tset_state\tEND\t{user}')
 
 
 def delete_state(user: int) -> None:
+    log(f'Database\tdelete_state\tSTART\t{user}')
     with psycopg2.connect(settings.db_parameters_string) as conn:
         cur = conn.cursor()
         delete_time_query = f'DELETE FROM states WHERE states.user_id = {user}'
         cur.execute(delete_time_query)
         conn.commit()
+    log(f'Database\tdelete_state\tEND\t{user}')
