a5acf7a35645fadd41a30199c945f0e6a67cb7be
Benyomin Ginzburg <benyomin.94@gmail.com>
2018-11-17 22:41:18 +0200

    Preparing for nginx

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ WEBHOOK_PORT = settings.BOT_PORT
 ssl_cert = '/hdd/certs/webhook_cert.pem'
 ssl_cert_key = '/hdd/certs/webhook_pkey.pem'
 base_url = f'{WEBHOOK_HOST}:{WEBHOOK_PORT}'
-route_path = settings.URI
+route_path = f'/settings.URI/'
 
 logger = logging.getLogger('bot_logger')
 logger.setLevel(logging.INFO)
