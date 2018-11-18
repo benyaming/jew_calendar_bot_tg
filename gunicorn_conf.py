c42649f122d05650e4c45a2571e7032361de6157
Benyomin Ginzburg <benyomin.94@gmail.com>
2018-11-18 15:40:51 +0200

    fix log path

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -2,4 +2,4 @@ import multiprocessing
 
 bind = '0.0.0.0:8443'
 workers = multiprocessing.cpu_count() * 2 + 1
-accesslog = '/home/jcb/etc/gunicorn_access_log'
+accesslog = '/home/jcb/log/gunicorn_access_log'
