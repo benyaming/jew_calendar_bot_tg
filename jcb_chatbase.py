94f7f8492dd66b832b4d75bf33eaebe0ebc3731c
Benyamin Ginzburg <benyomin.94@gmail.com>
2020-04-12 22:01:20 +0300

    Revert "Fixed bug with incorrect pesach 7th day date"
    This reverts commit f036c96e

--- jcb_chatbase.py
+++ jcb_chatbase.py
@@ -1,4 +1,4 @@
-# from chatbase import Message, MessageTypes
+from chatbase import Message, MessageTypes
 import time
 import settings
 
@@ -15,31 +15,29 @@ def chatbase_user_msg_handler(
         intent_message: str,
         not_handled=False
 ) -> None:
-    # user_message = Message(
-    #     api_key=API_KEY,
-    #     platform=PLATFORM,
-    #     version=VERSION,
-    #     time_stamp=TIME_STAMP,
-    #     type=MessageTypes.USER,
-    #     user_id=user_id,
-    #     message=message,
-    #     intent=intent_message,
-    #     not_handled=not_handled
-    # )
-    # user_message.send()
-    ...
+    user_message = Message(
+        api_key=API_KEY,
+        platform=PLATFORM,
+        version=VERSION,
+        time_stamp=TIME_STAMP,
+        type=MessageTypes.USER,
+        user_id=user_id,
+        message=message,
+        intent=intent_message,
+        not_handled=not_handled
+    )
+    user_message.send()
 
 
 # Handler for bot's message
 def chatbase_bot_handler(user_id: int, message: str) -> None:
-    # set_bot_message = Message(
-    #     api_key=API_KEY,
-    #     platform=PLATFORM,
-    #     version=VERSION,
-    #     time_stamp=TIME_STAMP,
-    #     type=MessageTypes.AGENT,
-    #     user_id=user_id,
-    #     message=message
-    # )
-    # set_bot_message.send()
-    ...
+    set_bot_message = Message(
+        api_key=API_KEY,
+        platform=PLATFORM,
+        version=VERSION,
+        time_stamp=TIME_STAMP,
+        type=MessageTypes.AGENT,
+        user_id=user_id,
+        message=message
+    )
+    set_bot_message.send()
